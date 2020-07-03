a44c31066e15fc000c71dc563071a19601c7d415
演得太逼真 <707335344@qq.com>
2020-07-03 10:35:03 +0800

    Update SpringCloudAppApiFeignApplication.java

--- spring-cloud-app-api-feign/src/main/java/com/liyuan/demo/SpringCloudAppApiFeignApplication.java
+++ spring-cloud-app-api-feign/src/main/java/com/liyuan/demo/SpringCloudAppApiFeignApplication.java
@@ -2,6 +2,7 @@ package com.liyuan.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -10,6 +11,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 //EnableFeignClients：表示开启Spring Cloud Feign的支持功能
 @EnableFeignClients
+//如果后面需要接入到hystrix-board监控，这边一定要添加熔断器注解
+@EnableCircuitBreaker
 public class SpringCloudAppApiFeignApplication {
 
     public static void main(String[] args) {
