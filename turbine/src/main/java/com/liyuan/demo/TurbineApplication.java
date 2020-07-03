59d32337cbe26049779fbbdf80f66beaac3ecd4d
演得太逼真 <707335344@qq.com>
2020-07-03 12:23:24 +0800

    Update TurbineApplication.java

--- turbine/src/main/java/com/liyuan/demo/TurbineApplication.java
+++ turbine/src/main/java/com/liyuan/demo/TurbineApplication.java
@@ -2,11 +2,17 @@ package com.liyuan.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @EnableTurbine
 public class TurbineApplication {
 
