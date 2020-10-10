f55ddc41ea107a500150f55fb3d2339b8fd171c5
演得太逼真 <707335344@qq.com>
2020-10-10 14:43:10 +0800

    Create UserFallbackFactory.java

---
+++ spring-cloud-app-api-feign/src/main/java/com/liyuan/demo/rest/impl/UserFallbackFactory.java
@@ -0,0 +1,39 @@
+package com.liyuan.demo.rest.impl;
+
+import com.liyuan.demo.res.BaseResVo;
+import com.liyuan.demo.rest.UserRest;
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName UserFallbackFactory
+ * @Description TODO
+ * @Author liyuan
+ * @Date 2020/3/3 22:15
+ **/
+@Slf4j
+@Component
+public class UserFallbackFactory implements FallbackFactory<UserRest> {
+    @Override
+    public UserRest create(Throwable throwable) {
+        return new UserRest() {
+            @Override
+            public BaseResVo queryByName(String name) {
+                return BaseResVo.builder().
+                        resCode(1005)
+                        .resMsg("用户服务不可用")
+                        .build();
+            }
+
+            @Override
+            public BaseResVo queryByIdsss(Integer id, String authorization) {
+                log.error("用户服务不可用，throwable={}",throwable);
+                return BaseResVo.builder().
+                        resCode(1005)
+                        .resMsg("用户服务不可用")
+                        .build();
+            }
+        };
+    }
+}
